53bd515a57619663d24d90933827f31c864e12b3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-27 12:38:41 -0700

    Added a code for local settings file

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Local settings file
+try:
+    from .local_settings import *
+except ImportError:
+    pass
