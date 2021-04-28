7d50160c8e273e8f70a8c95245599df8d6bb03e8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-28 12:18:51 -0700

    Installed and added django-phonenumber-field

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Installed Apps
+    'phonenumber_field',
     # My Apps
     'foodtruck',
     'user',
