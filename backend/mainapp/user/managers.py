b87af9937fe039b7acfbeb35fad0b7c25fe83b61
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-27 20:41:26 -0700

    Added a name field with validation, and stored it to DB

--- backend/mainapp/user/managers.py
+++ backend/mainapp/user/managers.py
@@ -7,23 +7,25 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers for authentication instead of usernames.
     """
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, name, email, password, **extra_fields):
         """
-        Create and save a User with the given email and password.
+        Create and save a User with the given name, email, and password.
         """
         if not email:
             raise ValueError(_('The email must be set.'))
+        if not name:
+            raise ValueError(_('The name must be set.'))
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(name=name, email=email, **extra_fields)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, name, email, password, **extra_fields):
         """
-        Create and save a SuperUser with the given email and password.
+        Create and save a SuperUser with the given name, email, and password.
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -34,4 +36,4 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('SuperUser must have is_superuser=True.'))
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(name, email, password, **extra_fields)
