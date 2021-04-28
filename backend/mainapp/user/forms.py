21f7395133d29846d9fe03d01fbc38d1663b5176
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-27 21:30:57 -0700

    Imported: UserCreationForm and UserChangeForm. Created: CustomerUserCreationForm and CustomUserChangeForm to subclasses them on using the new CustomUser model

---
+++ backend/mainapp/user/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    Subclassing UserCreationForm to use the new CustomUser model with fields of name, email, password1, and password2.
+    """
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('name', 'email', 'password1', 'password2',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """
+    Subclassing UserChangeForm to use the new CustomUser model with fields of name, email, password1, and password2.
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('name', 'email', 'password1', 'password2',)
