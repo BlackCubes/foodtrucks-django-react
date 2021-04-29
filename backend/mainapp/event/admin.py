5fac1f177c669f2e654fbfecc12eeb52cdb6315f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-29 12:37:11 -0700

    Imported: Event model. Created: a custom admin form for Event model. Registered: EventAdmin with Event model

--- backend/mainapp/event/admin.py
+++ backend/mainapp/event/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Event
+
 # Register your models here.
+
+
+# EVENT ADMIN
+class EventAdmin(admin.ModelAdmin):
+    """"""
+    list_filter = ('date',)
+
+    fieldsets = (
+        (None, {'fields': ('date', 'start_time', 'end_time',)}),
+        ('Geo Location', {'fields': ('geo_location',)}),
+        ('Truck', {'fields': ('truck',)}),
+    )
+
+    readonly_fields = ('uuid,')
+
+    search_fields = ('date',)
+
+
+admin.site.register(Event, EventAdmin)
