aeeefc5bf35c383f0e613a1fea59c185ff5ab2e6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-29 12:17:25 -0700

    Imported: ugettext_lazy and uuid. Created: an Event model with many-to-many relationship

--- backend/mainapp/event/models.py
+++ backend/mainapp/event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import uuid
 
 # Create your models here.
+
+
+# EVENTS
+class Event(models.Model):
+    """
+    Event model with fields of uuid, date, start_time, end_time, geo_location, and foodtruck. ForeignKey: Truck
+    """
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=True)
+    date = models.DateField(_('event date'))
+    start_time = models.TimeField(_('start time'))
+    end_time = models.TimeField(_('end time'))
+    geo_location = models.PointField(_('geo location'))
+    foodtruck = models.ManyToManyField('foodtruck.Truck')
