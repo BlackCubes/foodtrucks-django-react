ef9bb7c7abbf8c0496d10bb84f4140d8318cbc43
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-28 19:36:33 -0700

    Imported: ModelForm, Product, Truck, and Truck_Image. Created: TruckModalForm, TruckImageModalForm, and ProductModalForm

---
+++ backend/mainapp/foodtruck/forms.py
@@ -0,0 +1,35 @@
+from django.forms import ModelForm
+
+from .models import Product, Truck, Truck_Image
+
+# TRUCK MODAL FORM
+
+
+class TruckModalForm(ModelForm):
+    """
+    Modal Form class for Truck with fields of name, info, phone_number, email, and website.
+    """
+    class Meta:
+        model = Truck
+        fields = ('name', 'info', 'phone_number', 'email', 'website',)
+
+
+# TRUCK_IMAGE MODAL FORM
+class TruckImageModalForm(ModalForm):
+    """
+    Modal Form class for Truck_Image with field of image.
+    """
+    class Meta:
+        modal = Truck_Image
+        fields = ('image',)
+
+
+# PRODUCT MODAL FORM
+class ProductModalForm(ModalForm):
+    """
+    Modal Form class for Product with fields of name, info, image, price, quantity, is_available, and truck.
+    """
+    class Meta:
+        modal = Product
+        fields = ('name', 'info', 'image', 'price',
+                  'quantity', 'is_available',)
