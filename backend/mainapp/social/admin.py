34e6d55ccb24cd21ae12959bd11c67f4a67616a3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-29 06:34:10 -0700

    Fixed class names

--- backend/mainapp/social/admin.py
+++ backend/mainapp/social/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .forms import EmojiModalForm, LikeModalForm
+from .forms import EmojiModelForm, LikeModelForm
 from .models import Emoji, Like
 
 # Register your models here.
