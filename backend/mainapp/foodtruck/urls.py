b8abee138ebfe8b9202ae81d4bf0753cb731bda4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-30 15:34:25 -0700

    Fixed syntax error by removing string

--- backend/mainapp/foodtruck/urls.py
+++ backend/mainapp/foodtruck/urls.py
@@ -14,5 +14,5 @@ router.register(r'products', views.APIProductViewSet)
 
 # WIRE UP FOODTRUCK API USING AUTOMATIC URL ROUTING
 urlpatterns = [
-    path('', include('router.urls')),
+    path('', include(router.urls)),
 ]
