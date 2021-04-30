aae2d872f88d747316e828d2351df6fde41836d7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-29 20:11:41 -0700

    Added django.contrib.gis to INSTALLED_APPS

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     # Installed Apps
     'phonenumber_field',
     # My Apps
