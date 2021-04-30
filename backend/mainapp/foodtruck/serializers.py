067d9018ca7331bb2a99d1b583d7b86244d9a600
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-30 07:07:48 -0700

    Imported: serializers, Product model, Truck_Image model, and Truck model. Created: serializers on Truck, Truck_Image, and Product models

---
+++ backend/mainapp/foodtruck/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+
+from .models import Product, Truck_Image, Truck
+
+
+# TRUCK SERIALIZER
+class TruckSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer on Truck model.
+
+    Fields: uuid, name, slug, info, phone_number, email, website.
+    """
+    class Meta:
+        model = Truck
+        fields = ('uuid', 'name', 'slug', 'info',
+                  'phone_number', 'email', 'website',)
+
+
+# TRUCK_IMAGE SERIALIZER
+class Truck_ImageSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer on Truck_Image model.
+
+    Fields: uuid, image, is_profile_image.
+    """
+    class Meta:
+        model = Truck_Image
+        fields = ('uuid', 'image', 'is_profile_image',)
+
+
+# PRODUCT SERIALIZER
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer on Product model.
+
+    Fields: uuid, name, slug, info, image, price, quantity, is_available, and truck.
+    """
+    class Meta:
+        model = Product
+        # CHECK PROPER USER OF MANY-TO-ONE OUTPUT
+        fields = ('uuid', 'name', 'slug', 'info', 'image',
+                  'price', 'quantity', 'is_available', 'truck')
