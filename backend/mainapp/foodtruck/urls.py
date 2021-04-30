e1332afc1ffa9f4c41861d5136356c5f2e0aba2f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-30 11:50:38 -0700

    Imported: viewsets from rest_framework, Product model, Truck model, Truck_Image model, ProductSerializer, TruckSerializer, and Truck_ImageSerializer. Created: an API serializer for Truck, Truck_Image, and Product models using viewsets.ModelViewSet. Added: self-comment for later

---
+++ backend/mainapp/foodtruck/urls.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets
+
+from .models import Product, Truck, Truck_Image
+from .serializers import ProductSerializer, TruckSerializer, Truck_ImageSerializer
+
+
+# TRUCK VIEWS
+class APITruckViewSet(viewsets.ModelViewSet):
+    """
+    ModelViewSet for Truck.
+
+    Request Type: GET and POST.
+    """
+    queryset = Truck.objects.all().order_by('name')
+    serializer_class = TruckSerializer
+
+
+# TRUCK_IMAGE VIEWS
+# Find out about how to implement a MANY-TO-ONE situation
+class APITruck_ImageViewSet(viewsets.ModelViewSet):
+    """
+    ModelViewSet for Truck_Image.
+
+    Request Type: GET and POST.
+    """
+    queryset = Truck_Image.objects.all().order_by('is_profile_image')
+    serializer_class = Truck_ImageSerializer
+
+
+# PRODUCT VIEWS
+# Find out about how to implement a MANY-TO-ONE situation
+class APIProductViewSet(viewsets.ModelViewSet):
+    """
+    ModelViewSet for Product.
+
+    Request Type: GET and POST.
+    """
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerializer
