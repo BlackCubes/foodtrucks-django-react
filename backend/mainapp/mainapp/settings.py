9d49407834d5b0b7a732b4af6d48dc33d5513301
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-29 20:33:37 -0700

    Removed anything related to GeoDjango

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     # Installed Apps
     'phonenumber_field',
     # My Apps
