be1facd3081f52320180381d3ce964c9faab9a35
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 10:33:25 -0700

    Changed order of the code to pylint being mean

--- backend/mainapp/user/models.py
+++ backend/mainapp/user/models.py
@@ -1,7 +1,7 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-import uuid
 
 from .managers import CustomUserManager
 
