44ac4832ee13ed1c77df8d61311ebbe1a25e58d3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 11:46:12 -0700

    Imported: generics from rest_framework, Review model, and ReviewSerializer. Created: serializer class for Review model to retrieve lists/create new one, and to retrieve, update, or delete

--- backend/mainapp/review/views.py
+++ backend/mainapp/review/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Review
+from .serializers import ReviewSerializer
+
+
+# REVIEW VIEWS
+class ReviewListCreateAPIView(generics.ListCreateAPIView):
+    """
+    API view for Review to retrieve lists or create new.
+
+    Request Type: GET and POST.
+    """
+    queryset = Review.objects.all().order_by('created_at')
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API view for Review to retrieve, update, or delete.
+
+    LookUp Field: uuid.
+
+    Request Type: GET, PUT, PATCH, and DELETE.
+    """
+    queryset = Review.objects.all().order_by('created_at')
+    lookup_field = 'uuid'
+    serializer_class = ReviewSerializer
