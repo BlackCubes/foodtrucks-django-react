133c9126734256fa668a553d6e74df834e92a216
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 09:46:52 -0700

    Changed the views classes

--- backend/mainapp/foodtruck/urls.py
+++ backend/mainapp/foodtruck/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('products/<uuid:uuid>', views.ProductDetailsAPIView.as_view()),
     path('images/', views.TruckImageListCreateAPIView.as_view()),
     path('images/<uuid:uuid>', views.TruckImageDetailsAPIView.as_view()),
-    path('', views.ProductListCreateAPIView.as_view()),
-    path('<uuid:uuid>', views.ProductDetailsAPIView.as_view()),
+    path('', views.TruckListCreateAPIView.as_view()),
+    path('<uuid:uuid>', views.TruckDetailsAPIView.as_view()),
 ]
