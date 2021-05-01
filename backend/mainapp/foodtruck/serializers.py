5a0da2a04815a69fddb0cd5820163820e7eb02fc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-30 19:44:15 -0700

    Changed the serializer type to ModelSerializer for Product, TruckImage, and Truck models; and changed the placement of the code

--- backend/mainapp/foodtruck/serializers.py
+++ backend/mainapp/foodtruck/serializers.py
@@ -3,33 +3,8 @@ from rest_framework import serializers
 from .models import Product, TruckImage, Truck
 
 
-# TRUCK SERIALIZER
-class TruckSerializer(serializers.HyperlinkedModelSerializer):
-    """
-    Serializer on Truck model.
-
-    Fields: uuid, name, slug, info, phone_number, email, website.
-    """
-    class Meta:
-        model = Truck
-        fields = ('uuid', 'name', 'slug', 'info',
-                  'phone_number', 'email', 'website',)
-
-
-# TRUCKIMAGE SERIALIZER
-class TruckImageSerializer(serializers.HyperlinkedModelSerializer):
-    """
-    Serializer on TruckImage model.
-
-    Fields: uuid, image, is_profile_image.
-    """
-    class Meta:
-        model = TruckImage
-        fields = ('uuid', 'image', 'is_profile_image',)
-
-
 # PRODUCT SERIALIZER
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     """
     Serializer on Product model.
 
@@ -40,3 +15,28 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
         # CHECK PROPER USER OF MANY-TO-ONE OUTPUT
         fields = ('uuid', 'name', 'slug', 'info', 'image',
                   'price', 'quantity', 'is_available', 'truck',)
+
+
+# TRUCKIMAGE SERIALIZER
+class TruckImageSerializer(serializers.ModelSerializer):
+    """
+    Serializer on TruckImage model.
+
+    Fields: uuid, image, is_profile_image.
+    """
+    class Meta:
+        model = TruckImage
+        fields = ('uuid', 'image', 'is_profile_image',)
+
+
+# TRUCK SERIALIZER
+class TruckSerializer(serializers.ModelSerializer):
+    """
+    Serializer on Truck model.
+
+    Fields: uuid, name, slug, info, phone_number, email, website.
+    """
+    class Meta:
+        model = Truck
+        fields = ('uuid', 'name', 'slug', 'info',
+                  'phone_number', 'email', 'website',)
