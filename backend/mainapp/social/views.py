928ef0dfa1f0a12ca4bc30536a857e43097c2f11
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 12:57:50 -0700

    Imported: genericcs from rest_framework, Emoji model, Like model, EmojiSerializer, and LikeSerializer. Created: serializer classes for Emoji and Like models

--- backend/mainapp/social/views.py
+++ backend/mainapp/social/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Emoji, Like
+from .serializers import EmojiSerializer, LikeSerializer
+
+
+# EMOJI VIEWS
+class EmojiListAPIView(generics.ListAPIView):
+    """
+    API view for Emoji to retrieve lists.
+
+    Request Type: GET.
+    """
+    queryset = Emoji.objects.all()
+    serializer_class = EmojiSerializer
+
+
+class EmojiDetailsAPIView(generics.RetrieveAPIView):
+    """
+    API view for Emoji to retrieve.
+
+    LookUp Field: uuid.
+
+    Request Type: GET.
+    """
+    queryset = Emoji.objects.all()
+    lookup_field = 'uuid'
+    serializer_class = EmojiSerializer
+
+
+# LIKE VIEWS
+# FIND A WAY TO DYNAMICALLY CHANGE/UPDATE WITH TWO FOREIGN FIELDS
+class LikeListCreateAPIView(generics.ListCreateAPIView):
+    """"""
+    queryset = Like.objects.all().order_by('created_at')
+    serializer_class = LikeSerializer
+
+
+class LikeDetailsAPIView(generics.RetrieveUpdateAPIView):
+    """"""
+    queryset = Like.objects.all().order_by('created_at')
+    lookup_field = 'uuid'
+    serializer_class = LikeSerializer
