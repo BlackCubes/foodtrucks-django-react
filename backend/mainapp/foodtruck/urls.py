07d27dfce2653ea87516cad61aa78532d6e93635
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 21:16:28 -0700

    Changed the url dispatcher path

--- backend/mainapp/foodtruck/urls.py
+++ backend/mainapp/foodtruck/urls.py
@@ -6,7 +6,7 @@ from . import views
 # WIRE UP FOODTRUCK
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
-    path('products/<uuid:uuid>', views.ProductDetailsAPIView.as_view()),
+    path('products/<slug:slug>', views.ProductDetailsAPIView.as_view()),
     path('', views.TruckListAPIView.as_view()),
-    path('<uuid:uuid>', views.TruckDetailsAPIView.as_view()),
+    path('<slug:slug>', views.TruckDetailsAPIView.as_view()),
 ]
