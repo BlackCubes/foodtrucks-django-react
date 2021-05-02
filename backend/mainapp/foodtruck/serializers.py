60caf3b8fa2172a0d331a1ae06865afdbdcc69d7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 21:18:27 -0700

    Changed anything with uuid to slug

--- backend/mainapp/foodtruck/serializers.py
+++ backend/mainapp/foodtruck/serializers.py
@@ -12,17 +12,17 @@ class ProductSerializer(serializers.ModelSerializer):
     """
     Serializer on Product model.
 
-    LookUp Field: uuid.
+    LookUp Field: slug.
 
     Fields: uuid, name, slug, info, image, price, quantity, is_available, truck, and reviews.
     """
-    truck = serializers.CharField(source='truck.uuid')
+    truck = serializers.CharField(source='truck.slug')
     reviews = ReviewSerializer(many=True, read_only=True)
     likes = LikeSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
-        lookup_field = 'uuid'
+        lookup_field = 'slug'
         # CHECK PROPER USER OF MANY-TO-ONE OUTPUT
         fields = ('uuid', 'name', 'slug', 'info', 'image',
                   'price', 'quantity', 'is_available', 'truck', 'reviews', 'likes',)
@@ -33,11 +33,11 @@ class TruckImageSerializer(serializers.ModelSerializer):
     """
     Serializer on TruckImage model.
 
-    LookUp Field: uuid.
+    LookUp Field: slug.
 
     Fields: uuid, image, is_profile_image.
     """
-    truck = serializers.CharField(source='truck.uuid')
+    truck = serializers.CharField(source='truck.slug')
 
     class Meta:
         model = TruckImage
@@ -50,7 +50,7 @@ class TruckSerializer(serializers.ModelSerializer):
     """
     Serializer on Truck model.
 
-    LookUp Field: uuid.
+    LookUp Field: slug.
 
     Fields: uuid, name, slug, info, phone_number, email, website, products, images, and events.
     """
@@ -60,6 +60,6 @@ class TruckSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Truck
-        lookup_field = 'uuid'
+        lookup_field = 'slug'
         fields = ('uuid', 'name', 'slug', 'info',
                   'phone_number', 'email', 'website', 'products', 'images', 'events',)
