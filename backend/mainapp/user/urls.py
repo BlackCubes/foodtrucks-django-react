29ae64865f786960cf695c8963e7b0e9fb2d41e9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 20:59:47 -0700

    Fixed the correct view

--- backend/mainapp/user/urls.py
+++ backend/mainapp/user/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 # WIRE UP USER
 urlpatterns = [
-    path('', views.UserListCreateAPIView.as_view()),
+    path('', views.UserListAPIView.as_view()),
     path('<uuid:uuid>', views.UserDetailsAPIView.as_view()),
 ]
