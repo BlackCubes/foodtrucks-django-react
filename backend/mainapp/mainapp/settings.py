368a94845e16afc075e927b2fee641d69f500870
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 09:02:28 -0700

    Added: corsheaders to INSTALLED_APPS and its middleware to MIDDLEWARE. Configured: added a DEFAULT_PERMISSION_CLASSES with rest_framework and DEFAULT_AUTHENTICATION_CLASSES with simplejwt to REST_FRAMEWORK; and added localhost:3000 to CORS_ALLOWED_ORIGINS

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # Installed Apps
     'phonenumber_field',
     'rest_framework',
+    'corsheaders',
     # My Apps
     'event',
     'foodtruck',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,6 +134,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:3000',
+)
 
 # Image Upload
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
