89b6d7ca22ecb6be063bf8431fda65f7f5c77bc6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 16:09:18 -0700

    Imported: MyObtainTokenPairView custom subclass. Wired Up: login route

--- backend/mainapp/user/urls.py
+++ backend/mainapp/user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from authentication.views import MyObtainTokenPairView
+
 from . import views
 
 
 # WIRE UP USER
 urlpatterns = [
-    path('', views.UserListAPIView.as_view()),
+    path('login/', MyObtainTokenPairView.as_view()),
     path('<uuid:uuid>', views.UserDetailsAPIView.as_view()),
+    path('', views.UserListAPIView.as_view()),
 ]
