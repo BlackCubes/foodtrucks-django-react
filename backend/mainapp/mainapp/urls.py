7d664cf1d21843b911a0853502e5572db79347ca
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 16:15:32 -0700

    Refactored the urls for authentication

--- backend/mainapp/mainapp/urls.py
+++ backend/mainapp/mainapp/urls.py
@@ -15,18 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView, )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # API
+    path('api/v1/authentications/', include('authentication.urls')),
     path('api/v1/events/', include('event.urls')),
     path('api/v1/foodtrucks/', include('foodtruck.urls')),
     path('api/v1/reviews/', include('review.urls')),
     path('api/v1/socials/', include('social.urls')),
     path('api/v1/users/', include('user.urls')),
-    # SIMPLEJWT
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
