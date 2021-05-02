b3eeda69273fbb46e5044ee3b56018ca630d74ff
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-01 20:14:34 -0700

    Changed: docstring. Added: related_name and on_delete option in truck ManyToManyField

--- backend/mainapp/event/models.py
+++ backend/mainapp/event/models.py
@@ -6,10 +6,13 @@ from django.utils.translation import ugettext_lazy as _
 # EVENTS
 class Event(models.Model):
     """
-    Event model with fields of uuid, date, start_time, end_time, and foodtruck. ForeignKey: Truck.
+    Event model with fields of uuid, date, start_time, end_time, and foodtruck.
+
+    ManyToMany: Truck.
     """
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=True)
     date = models.DateField(_('event date'))
     start_time = models.TimeField(_('start time'))
     end_time = models.TimeField(_('end time'))
-    truck = models.ManyToManyField('foodtruck.Truck')
+    truck = models.ManyToManyField(
+        'foodtruck.Truck', related_name='images', on_delete=models.CASCADE)
