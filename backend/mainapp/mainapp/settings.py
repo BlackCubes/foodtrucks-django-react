5c7a63be99d5fa7a8ebe20404a50575396407032
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 10:09:53 -0700

    Changed the order of the environ importation due to pylint being picky

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
 import os
 from pathlib import Path
 from datetime import timedelta
+import environ
 
 env = environ.Env()
 environ.Env.read_env()
