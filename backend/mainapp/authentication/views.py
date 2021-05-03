60d00e9dc1c26edb61bef2159e1f9e695fcea24e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 18:59:16 -0700

    Imported: generics and status from rest_framework; AllowAny and IsAuthenticated from rest_framework.permissions; Response from rest_framework.response; APIView from rest_framework.views; RefreshToken from rest_framework_simplejwt.tokens; CustomUser model; ChangePassword and Register serializers. Created: three views for registering new users, changing user's password, and logging out user. Removed: custom token subclass

--- backend/mainapp/authentication/views.py
+++ backend/mainapp/authentication/views.py
@@ -1,12 +1,50 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import MyTokenObtainPairSerializer
+from user.models import CustomUser
+
+from .serializers import ChangePasswordSerializer, RegisterSerializer
 
 
-class MyObtainTokenPairView(TokenObtainPairView):
+class RegisterView(generics.CreateAPIView):
     """
-    Subclass for TokenObtainPairView.
+    API view for CustomUser to register new users.
+
+    Request Type: POST.
     """
     permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.CreateAPIView):
+    """
+    API view for CustomUser to change user's password.
+
+    Request Type: PATCH and PUT.
+    """
+    permission_classes = (IsAuthenticated,)
+    queryset = CustomUser.objects.all()
+    serializer_class = ChangePasswordSerializer
+
+
+class LogoutView(APIView):
+    """
+    An APIView for logging out. Blacklists tokens.
+
+    Request Type: POST.
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
