96229b6b82bcff6a4381ad7ec4aa8f4f4efe9a2a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 23:50:47 -0700

    Fixed import ordering and other details due to pylint being picky

--- backend/mainapp/mainapp/utils.py
+++ backend/mainapp/mainapp/utils.py
@@ -1,6 +1,6 @@
-from django.utils.text import slugify
 import random
 import string
+from django.utils.text import slugify
 
 
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
@@ -14,15 +14,16 @@ def unique_slug_generator(instance, new_slug=None):
     """
     Assumes your instance has a model with a slug field and a name character (char) field.
 
-    Returns: unique_slug_generator if the slug query exists, or the newly created slug that has been slugify.
+    Returns: unique_slug_generator if the slug query exists, or the newly created slug that
+    has been slugify.
     """
     if new_slug is not None:
         slug = new_slug
     else:
         slug = slugify(instance.name)
 
-    Klass = instance.__class__
-    qs_exists = Klass.objects.filter(slug=slug).exists()
+    klass = instance.__class__
+    qs_exists = klass.objects.filter(slug=slug).exists()
 
     if qs_exists:
         new_slug = "{slug}-{randstr}".format(slug=slug,
