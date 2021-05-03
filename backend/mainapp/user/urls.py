c422373d0efec88227e5c8753901c67ef3e4b4a7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 18:39:48 -0700

    Refactored by removing the custom token subclass, and added route for refresh tokens

--- backend/mainapp/user/urls.py
+++ backend/mainapp/user/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
-from authentication.views import MyObtainTokenPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 
 # WIRE UP USER
 urlpatterns = [
-    path('login/', MyObtainTokenPairView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh')
     path('<uuid:uuid>', views.UserDetailsAPIView.as_view()),
     path('', views.UserListAPIView.as_view()),
 ]
