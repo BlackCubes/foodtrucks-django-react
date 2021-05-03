eaf0f514a1fc749a891cffea1a9a24b17afd8b62
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 21:19:15 -0700

    Removed unused import

--- backend/mainapp/social/views.py
+++ backend/mainapp/social/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from .models import Emoji, Like
 from .serializers import EmojiSerializer, LikeSerializer
