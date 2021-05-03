80935027c506e929b6eaae8e5cf53d790f5af6ec
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-02 18:40:00 -0700

    Fixed syntax error

--- backend/mainapp/user/urls.py
+++ backend/mainapp/user/urls.py
@@ -7,7 +7,7 @@ from . import views
 # WIRE UP USER
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('<uuid:uuid>', views.UserDetailsAPIView.as_view()),
     path('', views.UserListAPIView.as_view()),
 ]
