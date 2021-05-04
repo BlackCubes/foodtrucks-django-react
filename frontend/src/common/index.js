419838f63585fd3d78a4c64c813f98c9032a25f6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-04 07:32:55 -0700

    Imported/Exported: Image and Link

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,6 @@
 import Button from './Button';
 import { FormStyled, FormGroupStyled } from './FormStyled';
+import Image from './Image';
+import Link from './Link';
 
-export { Button, FormGroupStyled, FormStyled };
+export { Button, FormGroupStyled, FormStyled, Image, Link };
