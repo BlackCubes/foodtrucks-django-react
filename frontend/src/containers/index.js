4cafa8f5b32c5877737bb99e11a1f82a6f8f1d12
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-04 10:25:07 -0700

    Imported/Exported: footer container

--- frontend/src/containers/index.js
+++ frontend/src/containers/index.js
@@ -1,5 +1,6 @@
+import FooterContainer from './FooterContainer';
 import FormContainer from './FormContainer';
 import LoginPage from './LoginPage';
 import NavbarContainer from './NavbarContainer';
 
-export { FormContainer, LoginPage, NavbarContainer };
+export { FooterContainer, FormContainer, LoginPage, NavbarContainer };
