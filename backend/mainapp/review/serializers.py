b9a5195f5298f4991e1a07bc9ce8c09d9c0d7257
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-05 12:12:35 -0700

    Modified by using SlugRelatedField

--- backend/mainapp/review/serializers.py
+++ backend/mainapp/review/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from foodtruck.models import Product
+from user.models import CustomUser
 from .models import Review
 
 
@@ -12,8 +14,12 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     Fields: uuid, review, created_at, updated_at, product, and user.
     """
-    product = serializers.CharField(source='product.slug')
-    user = serializers.CharField(source='user.uuid')
+    # product = serializers.CharField(source='product.slug')
+    # user = serializers.CharField(source='user.uuid')
+    product = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Product.objects.all())
+    user = serializers.SlugRelatedField(
+        slug_field='uuid', queryset=CustomUser.objects.all())
 
     class Meta:
         model = Review
