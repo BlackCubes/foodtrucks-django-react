d1bfe59608d1fc39488e829af808dd38b8c6c664
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-04 21:32:32 -0700

    Fixed grammar and syntax error

--- backend/mainapp/user/signals.py
+++ backend/mainapp/user/signals.py
@@ -9,7 +9,7 @@ from .models import CustomUser
 @receiver(post_save, sender=CustomUser)
 def send_mail_on_create(sender, instance, **kwargs):
     """
-    Send email to useer upon registration.
+    Send email to user upon registration.
     """
     user = instance
     message = "Welcome to Foodtrucks App! We are pleased to have you! {user.name}"
@@ -17,7 +17,7 @@ def send_mail_on_create(sender, instance, **kwargs):
         subject='Welcome to Foodtrucks!',
         body=message,
         from_email=EMAIL_FROM,
-        to=[user.email]
+        to=[user.email],
         reply_to=[EMAIL_FROM],
     )
     return mail.send()
