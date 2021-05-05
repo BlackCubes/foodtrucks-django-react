bfe778e4157148765943386173b3d2fecbbcf4cc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-05 00:22:43 -0700

    Added jet theme colors

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -90,6 +90,40 @@ TEMPLATES = [
     },
 ]
 
+# JET
+JET_THEMES = [
+    {
+        'theme': 'default',  # theme folder name
+        'color': '#47bac1',  # color of the theme's button in user menu
+        'title': 'Default'  # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
 WSGI_APPLICATION = 'mainapp.wsgi.application'
 
 
