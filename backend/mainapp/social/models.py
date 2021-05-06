d1969bcc512b64aaf7ebd9ac28db89a7271f640f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-05 17:40:22 -0700

    Fixed syntax errors

--- backend/mainapp/social/models.py
+++ backend/mainapp/social/models.py
@@ -40,4 +40,4 @@ class Like(models.Model):
         'foodtruck.Product', related_name='likes', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{this.like} {this.emoji} for {this.product}'
+        return f'{self.like} {self.emoji} for {self.product}'
