91d52b978bcc0d08220f1d62fee0c7b8761369f0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-14 12:12:36 -0700

    Imported/Exported: ReviewContext and useReviewContext

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -1,10 +1,13 @@
 import AuthContext, { useAuthContext } from './AuthContext';
+import ReviewContext, { useReviewContext } from './ReviewContext';
 import SocialContext, { useSocialContext } from './SocialContext';
 import TruckContext, { useTruckContext } from './TruckContext';
 
 export {
   AuthContext,
   useAuthContext,
+  ReviewContext,
+  useReviewContext,
   SocialContext,
   useSocialContext,
   TruckContext,
