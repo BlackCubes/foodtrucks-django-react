69b08c536ab4e771b44298ce793ea269d1315191
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-16 12:40:07 -0700

    Removed files related to styling

--- frontend/src/common/index.js
+++
@@ -1,7 +0,0 @@
-import Button from './Button';
-import { FormStyled, FormGroupStyled } from './FormStyled';
-import Image from './Image';
-import Link from './Link';
-import SnackbarStyled from './SnackbarStyled';
-
-export { Button, FormGroupStyled, FormStyled, Image, Link, SnackbarStyled };
