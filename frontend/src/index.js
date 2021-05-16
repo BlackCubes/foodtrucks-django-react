fb47b4b3c3544d93aa8fe814e2d81eb209c0e3a9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-16 11:58:27 -0700

    Imported: tailwind.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/tailwind.css';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
