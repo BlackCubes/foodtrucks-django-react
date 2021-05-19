7b09f1a8dba78ef526d20ffdee943abf6c29889d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-18 20:33:56 -0700

    Imported: useState from react, and validateForm from utils folder. Created: values and errors state, handleTestChange function, and onTestSubmit function. Rendered: a login form placeholder for re-styling with TailwindCSS

--- frontend/src/containers/LoginPage.js
+++ frontend/src/containers/LoginPage.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { useAuthContext } from '../context';
+
+import { validateForm } from '../utils';
 
 const loginFormFields = [
   {
@@ -33,11 +35,27 @@ const loginFormFields = [
 
 const LoginPage = ({ FormContainerComponent }) => {
   const { checkAuth, login, apiAuthErr } = useAuthContext();
+  const { values, setValues } = useState({});
+  const { errors, setErrors } = useState({});
   const history = useHistory();
 
   if (checkAuth()) history.push('/');
 
   const onSubmission = (data) => login(data);
+
+  const handleTestChange = (e) => {
+    const { name, value } = e.target;
+
+    validateForm(name, value, setErrors);
+
+    setValues((val) => ({ ...val, [name]: value }));
+  };
+
+  const onTestSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e);
+  };
 
   useEffect(() => {
     document.title = 'Foodtrucks | Login';
@@ -49,6 +67,46 @@ const LoginPage = ({ FormContainerComponent }) => {
         onSubmit={onSubmission}
         formFields={loginFormFields}
       />
+
+      <form onSubmit={onTestSubmit} noValidate>
+        <div>
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              values={values.email || ''}
+              onChange={handleTestChange}
+              required
+            />
+
+            <span data-text="email">Email</span>
+
+            <span>{errors.email || 'Noice!'}</span>
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              values={values.password || ''}
+              onChange={handleTestChange}
+              required
+            />
+
+            <span data-text="password">Password</span>
+
+            <span>{errors.password || 'Noice!'}</span>
+          </label>
+        </div>
+
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
 
       {!apiAuthErr ? null : apiAuthErr}
     </>
