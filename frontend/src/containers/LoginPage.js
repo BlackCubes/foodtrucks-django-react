1202da62c3ef33cc1f7ba936726d7c6667b524cf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-21 06:46:11 -0700

    Added Tailwind Classes: border-solid and border-blue-300 for button

--- frontend/src/containers/LoginPage.js
+++ frontend/src/containers/LoginPage.js
@@ -116,7 +116,7 @@ const LoginPage = ({ FormContainerComponent }) => {
         <div className="mt-6">
           <button
             type="submit"
-            className="border text-xl w-full py-2 px-4 rounded-lg text-center"
+            className="border border-solid border-blue-300 text-xl w-full py-2 px-4 rounded-lg text-center"
           >
             Login
           </button>
