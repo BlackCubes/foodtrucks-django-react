36caf1d88475891a923e5c73ab2d96fe91089787
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-21 18:46:13 -0700

    Added Tailwind Classes: focus:outline-none, focus:ring-2, focus:ring-blue-300, focus:boder-transparent to button

--- frontend/src/containers/LoginPage.js
+++ frontend/src/containers/LoginPage.js
@@ -121,7 +121,7 @@ const LoginPage = ({ FormContainerComponent }) => {
           <div className="mt-6">
             <button
               type="submit"
-              className="border border-solid border-blue-900 text-xl w-full py-2 px-4 rounded-lg shadow-lg text-center hover:border-0 hover:bg-blue-900 hover:text-white"
+              className="border border-solid border-blue-900 text-xl w-full py-2 px-4 rounded-lg shadow-lg text-center hover:border-0 hover:bg-blue-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent"
             >
               Login
             </button>
