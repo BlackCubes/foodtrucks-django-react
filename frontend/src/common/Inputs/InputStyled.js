cae95e49e4cb6586bf0e86949a6bed99d7f249f2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 15:21:14 -0700

    Added Tailwind Classes: dynamically added ring-2, ring-red-500, border-transparent, and ring-blue-300 on whether there are any errors

--- frontend/src/common/Inputs/InputStyled.js
+++ frontend/src/common/Inputs/InputStyled.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export default styled.input.attrs((props) => ({
-  className: `border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 ${
-    props.error ? 'focus:ring-red-500' : 'focus:ring-blue-300'
-  } focus:border-transparent`,
+  className: `border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:border-transparent ${
+    props.error
+      ? 'ring-2 ring-red-500 border-transparent focus:ring-red-500'
+      : 'ring-2 ring-blue-300 border-transparent focus:ring-blue-300'
+  }`,
 }))`
   &.not-empty ~ span[data-text] {
     font-size: 1.1rem;
