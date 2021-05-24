4d2768c65027670db1832f018d7cc188d989d049
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 15:22:41 -0700

    Removed Tailwind Classes: ring-2, ring-blue-300, and border-transparent on the last ternary conditional

--- frontend/src/common/Inputs/InputStyled.js
+++ frontend/src/common/Inputs/InputStyled.js
@@ -4,7 +4,7 @@ export default styled.input.attrs((props) => ({
   className: `border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:border-transparent ${
     props.error
       ? 'ring-2 ring-red-500 border-transparent focus:ring-red-500'
-      : 'ring-2 ring-blue-300 border-transparent focus:ring-blue-300'
+      : 'focus:ring-blue-300'
   }`,
 }))`
   &.not-empty ~ span[data-text] {
