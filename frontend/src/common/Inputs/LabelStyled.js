5b64e1d46a02527ff0a275d88b434a7e30e42ac7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 13:52:25 -0700

    Added backticks

--- frontend/src/common/Inputs/LabelStyled.js
+++ frontend/src/common/Inputs/LabelStyled.js
@@ -3,4 +3,4 @@ import styled from 'styled-components';
 export default styled.label.attrs((props) => ({
   className: 'flex flex-col relative mb-2',
   htmlFor: props.htmlFor,
-}));
+}))``;
--- frontend/src/common/Inputs/MessageStyled.js
+++ frontend/src/common/Inputs/MessageStyled.js
@@ -2,4 +2,4 @@ import styled from 'styled-components';
 
 export default styled.span.attrs(() => ({
   className: 'text-base mt-2 px-4',
-}));
+}))``;
