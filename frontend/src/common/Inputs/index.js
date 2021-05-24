913821b7737876e4ac9d53aec373d9746e70e892
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 12:37:05 -0700

    Imported/Exported: InputStyled, LabelStyled, MessageStyled, and PlaceholderStyled

---
+++ frontend/src/common/Inputs/index.js
@@ -0,0 +1,6 @@
+import InputStyled from './InputStyled';
+import LabelStyled from './LabelStyled';
+import MessageStyled from './MessageStyled';
+import PlaceholderStyled from './PlaceholderStyled';
+
+export { InputStyled, LabelStyled, MessageStyled, PlaceholderStyled };
