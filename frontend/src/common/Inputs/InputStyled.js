72749010cea536a0fc4a135307f3ddbc28a6811c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 15:05:29 -0700

    Dynamically changed the ring color on focus based on any errors

--- frontend/src/common/Inputs/InputStyled.js
+++ frontend/src/common/Inputs/InputStyled.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
-export default styled.input.attrs(() => ({
-  className:
-    'border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent',
+export default styled.input.attrs((props) => ({
+  className: `border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 ${
+    props.error ? 'focus:ring-red-500' : 'focus:ring-blue-300'
+  } focus:border-transparent`,
 }))`
   &.not-empty ~ span[data-text] {
     font-size: 1.1rem;
