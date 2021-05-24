b0142160366d139f477e804774b878a17b32ef44
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 09:55:25 -0700

    Imported: styled from styled-components. Created/Exported: an input custom styled component

---
+++ frontend/src/common/Inputs/InputStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.input.attrs(() => ({
+  className:
+    'border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent',
+}))`
+  &.not-empty ~ span[data-text] {
+    font-size: 1.1rem;
+    top: -30px;
+  }
+`;
