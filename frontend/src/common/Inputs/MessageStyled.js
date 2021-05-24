03bdb674d513f71cbd5d630eed37dcaddd502c54
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-24 14:50:28 -0700

    Added props with conditional that checks if there are errors, and if there is then display a red color

--- frontend/src/common/Inputs/MessageStyled.js
+++ frontend/src/common/Inputs/MessageStyled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
-export default styled.span.attrs(() => ({
-  className: 'text-xs mt-2 px-4',
+export default styled.span.attrs((props) => ({
+  className: `text-xs mt-2 px-4 ${props.error ? 'text-red-500' : null}`,
 }))``;
