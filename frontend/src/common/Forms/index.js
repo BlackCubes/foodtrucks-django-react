f4dbfc903ea80a5910eaf5c24acc6733791e1dbf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-25 11:19:47 -0700

    Imported/Exported: FormContainerStyled, FormGroupStyled, FormStyled, and FormTitleStyled

---
+++ frontend/src/common/Forms/index.js
@@ -0,0 +1,6 @@
+import FormContainerStyled from './FormContainerStyled';
+import FormGroupStyled from './FormGroupStyled';
+import FormStyled from './FormStyled';
+import FormTitleStyled from './FormTitleStyled';
+
+export { FormContainerStyled, FormGroupStyled, FormStyled, FormTitleStyled };
