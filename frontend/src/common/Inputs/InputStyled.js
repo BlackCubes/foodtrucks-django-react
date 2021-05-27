55e6545b33bac0e87693f0f05a97406bf23b82b2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-27 06:03:52 -0700

    Changed TailwindCSS: text-xl to text-lg

--- frontend/src/common/Inputs/InputStyled.js
+++ frontend/src/common/Inputs/InputStyled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.input.attrs((props) => ({
-  className: `border text-xl py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:border-transparent ${
+  className: `border text-lg py-2 px-4 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:border-transparent ${
     props.error
       ? 'ring-2 ring-red-500 border-transparent focus:ring-red-500'
       : 'focus:ring-blue-300'
